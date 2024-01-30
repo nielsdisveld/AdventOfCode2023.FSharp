bdb5b447a7d75c035a5e26c8b305895e2ed3595e
nielsdisveld <Niels.Disveld@finaps.nl>
2024-01-30 16:23:35 +0100

    day15 part1

---
+++ day15/Program.fs
@@ -0,0 +1,16 @@
+﻿let parseLine (str: string) = str.Split ','
+
+let hash (str: string) =
+    let folder acc c =
+        acc |> (fun a -> a + int c) |> (fun a -> 17 * a) |> (fun a -> a % 256)
+
+    str |> Seq.fold folder 0
+
+let solve =
+    Utils.FileReading.readLines
+    >> Seq.head
+    >> parseLine
+    >> Array.map hash
+    >> Array.sum
+
+solve "input.txt" |> printfn "%A"
